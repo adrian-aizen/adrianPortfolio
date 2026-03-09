33fb52b496d116001fccac2a47fcb858f6c28584
Adrian Adora <aidss24adora@gmail.com>
2026-03-09 14:53:55 +0800

    Progress: Installed React as Frontend Framework and Configured TailwindCSS

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-white text-4xl font-bold">My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
