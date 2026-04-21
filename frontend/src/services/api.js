e97608c58f907961214253527d777f15bff73e89
Adrian Adora <aidss24adora@gmail.com>
2026-04-21 11:58:05 +0800

    Fixed API URL for Production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://adrianportfolio.onrender.com";
 
 export const sendMessage = async ({ name, email, message }) => {
   const response = await fetch(`${API_URL}/api/contact`, {
